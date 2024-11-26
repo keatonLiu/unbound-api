fae4eb713576c92c48d2e4cab7522578e674a5e5
刘晓彤 <653603927@qq.com>
2024-11-26 16:36:05 +0800

    rm .venv

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/env bash
 DEBIAN_FRONTEND=noninteractive sudo apt update
 DEBIAN_FRONTEND=noninteractive sudo apt install -y python3.10-venv
-if [ ! -d ".venv" ]; then
-    python3 -m venv .venv
-fi
+rm -r .venv
+python3 -m venv .venv
 source .venv/bin/activate && pip install -r requirements.txt
